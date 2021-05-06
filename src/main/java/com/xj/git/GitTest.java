23406657fea2ced908bb8132db7bb152ebf813bf
Yuanf <Yuanf@163.com>
2021-05-06 15:14:06 +0800

    push commit

--- src/main/java/com/xj/git/GitTest.java
+++ src/main/java/com/xj/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("master test!");
         System.out.println("hotfix test!");
+        System.out.println("push test!");
     }
 }
